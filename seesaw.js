7161ea35869195309c8997ebf86488d860b92a4e
Beyza Cankurtaran <byzcnkrtrn@gmail.com>
2026-03-03 22:49:22 +0300

    trying to build

---
+++ seesaw.js
@@ -0,0 +1,9 @@
+const plank = document.getElementById("plank");
+const leftWeightEl = document.getElementById("left-weight-value");
+const rightWeightEl = document.getElementById("right-weight-value");
+const angleValueEl = document.getElementById("tilt-angle-value");
+const resetBtn = document.getElementById("resetBtn");
+const nextWeightEl = document.getElementById("nextWeight");
+const leftTorqueEl = document.getElementById("left-tork-value");
+const rightTorqueEl = document.getElementById("right-tork-value");
+
